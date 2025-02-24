73b2220d4007084cdb1ee856c5e3bc84000d7baf
HAtif Shaikh <hatifshaikh80@gmail.com>
2025-02-25 00:57:12 +0530

    first commit

---
+++ ES6/index.js
@@ -0,0 +1,14 @@
+// Tamplet
+const name = 'John';
+const age = 25;
+console.log("Hello Mr " + name + " your age is:" + age );
+
+console.log(`Hello Mr ${name} your age is:${age}`);
+
+
+// Q. Sum of two numbers
+const a = 10;
+const b = 20;
+console.log("Sum of "+a+" and b "+b+" is:"+a+b);
+console.log("Sum of "+a+" and b "+b+" is:"+(a+b));
+console.log(`Sum of ${a} and ${b} is:${a+b}`);
